c9ff941d46ae9fe82929e6adf37aeac14cbab64b
Yuurij-dot <yurijesusgoncalves@gmail.com>
2025-03-13 15:41:26 -0300

    Emplementação de adicionar produtos

--- src/Components/TableProdutos/index.jsx
+++ src/Components/TableProdutos/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 import {
   Table,
@@ -10,13 +10,22 @@ import {
   Paper,
   IconButton,
 } from "@mui/material";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
+import { v4 as uuidv4 } from "uuid";
+
 import { Edit, Delete } from "@mui/icons-material";
 
+
 const TableProdutos = () => {
+
+  // Estado que controla a exebição do modal
   const [produtos, setProdutos] = useState([
     { id: 1, nome: "Carrinho", preco: "R$ 50,00", estoque: 500, nivelEstoque: "Alto", dataCriacao: "27/02/2025" },
     { id: 2, nome: "Vassoura", preco: "R$ 2,00", estoque: 122, nivelEstoque: "Medio", dataCriacao: "25/02/2025" },
-    { id: 3, nome: "Caneta", preco: "R$ 12,00", estoque: 345, nivelEstoque: "Alto", dataCriacao: "12/11/2024" },
+    { id: 3, nome: "Caneta", preco: "R$ 12,00", estoque: 345, nivelEstoque: "Medio", dataCriacao: "12/11/2024" },
     { id: 4, nome: "Balinha", preco: "R$ 1,00", estoque: 10, nivelEstoque: "Baixo", dataCriacao: "12/11/2024" },
   ]);
 
@@ -41,8 +50,59 @@ const TableProdutos = () => {
     }
   };
 
+  // Estado para controlar a exibição do modal add produto
+  const [modalAddProduto, setModalAddProduto] = useState(null)
+
+  //Função para exibir modal add produto
+  const showAddProduct = (produto) => {
+      setModalAddProduto(produto)
+  }
+
+  // Função para fechar o modal add produto
+  const closeAddProduct = () => {
+    setModalAddProduto(null)
+  }
+
+  // Função para pegar o valores dos inputs de adiconar produto
+  const [nameProductUse, setNameProduct] = useState('')
+  const [priceProductUse, setPriceProduct] = useState('')
+  const [estoqueProductUse, setEstoqueProduct] = useState('')
+
+  const handleNameProduct = (e) => {
+    let nameProduct = e.target.value 
+    setNameProduct(nameProduct)
+  }
+
+  const handlePriceProduct = (e) => {
+    let priceProduct = e.target.value 
+    setPriceProduct(priceProduct)
+  }
+
+  const handleEstoqueProduct = (e) => {
+    let estoqueProduct = e.target.value 
+    setEstoqueProduct(estoqueProduct)
+  }
+
+  // Função para criar Produto
+  const addProduct = () => {
+    const newProduto = {
+      id: uuidv4(),
+      nome: nameProductUse,
+      preco: priceProductUse,
+      estoque: estoqueProductUse,
+      nivelEstoque: estoqueProductUse >= 500 ? "Alto" : estoqueProductUse > 200 ? "Medio" : "Baixo",
+      dataCriacao: new Date().toLocaleDateString()
+    }
+
+    setProdutos((prevProduto) =>  [...prevProduto, newProduto])
+    closeAddProduct()
+  }
   return (
     <div className="container-table">
+      <header className="flex items-center justify-between">
+        <h1 className="text-3xl text-neutral-800  font-bold">Produtos</h1>
+        <button onClick={showAddProduct} className="button-produtos-header bg-neutral-800 rounded text-white text-sm flex items-center gap-3.5 cursor-pointer hover:opacity-70"><FontAwesomeIcon icon={faPlus} className="text-sm" /> Novo Produto</button>
+      </header>
       <TableContainer className="table-container rounded-lg" component={Paper}>
         <Table>
           <TableHead>
@@ -99,6 +159,35 @@ const TableProdutos = () => {
             <div className="flex justify-end gap-2 mt-4">
               <button className="bg-white text-zinc-600 text-sm px-4 py-2 rounded" onClick={closeRemoveConfirme}>Cancelar</button>
               <button className="bg-red-500 text-white text-sm px-4 py-2 rounded" onClick={handleRemove}>Confirmar</button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {modalAddProduto && (
+        <div className="modal-overlay">
+          <div className="add-product-box">
+            <h1>Adicionar Produto</h1>
+            <div className="flex flex-col gap-5">
+              <div className="flex flex-col gap-2">
+                <span className="text-zinc-800 text-sm font-bold">Nome</span>
+                <input onChange={handleNameProduct} type="text" placeholder="Meu Produto" />
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <span className="text-zinc-800 text-sm font-bold">Preço</span>
+                <input onChange={handlePriceProduct} type="number" placeholder="R$ 0,00" />
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <span className="text-zinc-800 text-sm font-bold">Estoque</span>
+                <input onChange={handleEstoqueProduct} type="number" placeholder="0" />
+              </div>
+            </div>
+            
+            <div className="buttons-add-product-box flex justify-end gap-2 mt-4">
+              <button className="bg-white border  text-zinc-600 text-sm px-4 py-2 rounded" onClick={closeAddProduct}>Cancelar</button>
+              <button className="bg-zinc-800 text-white text-sm px-4 py-2 rounded" onClick={addProduct}>Confirmar</button>
             </div>
           </div>
         </div>
